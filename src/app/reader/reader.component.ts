54fa95c32805a131c17f04d993cb9f8344b3179b
Dagzy <quincye.williams@gmail.com>
2018-02-14 11:24:05 -0500

    QW xkcd api reader functional

--- src/app/reader/reader.component.ts
+++ src/app/reader/reader.component.ts
@@ -8,24 +8,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ReaderComponent implements OnInit {
   results: any;
+  data = {img: ''};
   arr = [];
+  objy = {piccy: ''};
+  API_BASE = 'https://xkcd.now.sh/';
+  plicky: any;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    console.log(this.http);
-
+    this.http.get(this.API_BASE).subscribe(data => {
+      data = data;
+      this.results = data;
+      this.plicky = this.results.img;
+      const quicky = document.getElementById('picz');
+      quicky.setAttribute('src', this.plicky);
+    });
   }
   gettyClicky() {
-    this.http.get('http://demo.ckan.org/api/3/action/package_search?fq=tags:economy').subscribe(data => {
-      this.results = data;
-      console.log(typeof(this.results));
-      console.log(this.results);
-    //   console.log(this.results[1].login);
-    //   this.results.forEach(element => {
-    //     console.log(element.login);
-    //     this.arr.push(element.login);
-    //   });
-    // console.log(this.arr[0], this.arr[1]);
+    let n = Math.random();
+    n = n += .5;
+    n = n * 1000;
+    n = Math.floor(n);
+    this.http.get(this.API_BASE + n).subscribe(data => {
+    this.results = data;
+    const quicky = document.getElementById('picz');
+    quicky.setAttribute('src', this.results.img);
+
     });
   }
 
